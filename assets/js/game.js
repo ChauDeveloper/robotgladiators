5ee7259a08ffeb0372761c07aa5b3aa38419e0e6
Chau <chau@Chaus-MacBook-Pro.local>
2020-11-11 22:55:45 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,14 +13,13 @@ if (playerHealth > 0) {
     }
 var playerAttack = 10;
 var playerMoney = 10;
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
   // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -61,7 +60,9 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
   // if no (false), ask question again by running fight() again
   else {
-    fight();
-  }
+    for(var i = 0; i < enemyNames.length; i++) {
+      fight(enemyNames[i]);
+    }
+}
 }
 }
